bd97db365eb2c3cc226391c305344e75af88e941
Marwan Taher <marokhaled99@gmail.com>
2020-12-04 04:58:51 +0000

    some formatting and cleanup

--- include/pointcloud_align/misaligned.hpp
+++ include/pointcloud_align/misaligned.hpp
@@ -2,12 +2,10 @@
 
 #include <ros/ros.h>
 
-// PCL specific includes
-#include <sensor_msgs/PointCloud2.h>
-#include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-// ICP
+#include <pcl_conversions/pcl_conversions.h>
+
 #include <pcl/io/ply_io.h>
 #include <pcl/point_types.h>
 #include <pcl/registration/icp.h>
@@ -17,37 +15,37 @@
 typedef pcl::PointXYZ PointT;
 typedef pcl::PointCloud<PointT> PointCloudT;
 
-class misaligned 
+class misaligned
 {
-    ros::NodeHandle& nh_;
-    ros::Publisher pc_pub_;
-    ros::Publisher path_pub_;
-    ros::Publisher pc_pcd_pub_;
+  ros::NodeHandle& nh_;
+  ros::Publisher pc_pub_;
+  ros::Publisher path_pub_;
+  ros::Publisher pc_pcd_pub_;
 
-    std::string name_;
-    std::string bag_name_;
-    std::string pcd_in_dir_;
-    std::string pcd_out_dir_;
-    
-    int iterations_;
-    int correspondence_;
-    int ransac_;
+  std::string name_;
+  std::string bag_name_;
+  std::string pcd_in_dir_;
+  std::string pcd_out_dir_;
 
-    double initial_x_;
-    double initial_y_;
-    double initial_rot_;
+  int iterations_;
+  int correspondence_;
+  int ransac_;
 
-    PointCloudT::Ptr baseline_cloud_;
-    PointCloudT::Ptr experiment_cloud_;
+  double initial_x_;
+  double initial_y_;
+  double initial_rot_;
 
-    nav_msgs::Path path_;
+  PointCloudT::Ptr baseline_cloud_;
+  PointCloudT::Ptr experiment_cloud_;
+
+  nav_msgs::Path path_;
 
 public:
-    misaligned(ros::NodeHandle & nh, std::string param_prefix, PointCloudT::Ptr baseline_cloud);
+  misaligned(ros::NodeHandle& nh, std::string param_prefix, PointCloudT::Ptr baseline_cloud);
 
-    bool icp();
-    void write_to_csv(std::string dir);
-    void transform_pcd_files();
-    
-    bool transform_pcd;
+  bool icp();
+  void write_to_csv(std::string dir);
+  void transform_pcd_files();
+
+  bool transform_pcd;
 };
--- src/misaligned.cpp
+++ src/misaligned.cpp
@@ -1,76 +1,75 @@
 #include <fstream>
 
-
-#include <tf/tf.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-// rosbag includes
 #include <rosbag/bag.h>
 #include <rosbag/view.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <tf/tf.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
-#include <pcl/io/pcd_io.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/io/pcd_io.h>
 #include <pointcloud_align/misaligned.hpp>
 
-misaligned::misaligned(ros::NodeHandle & nh, std::string param_prefix, PointCloudT::Ptr baseline_cloud)
-:   nh_(nh),
-    name_(param_prefix),
-    baseline_cloud_(baseline_cloud),
-    experiment_cloud_(new PointCloudT)
+misaligned::misaligned(ros::NodeHandle& nh, std::string param_prefix, PointCloudT::Ptr baseline_cloud)
+  : nh_(nh), name_(param_prefix), baseline_cloud_(baseline_cloud), experiment_cloud_(new PointCloudT)
 {
-    ROS_INFO("Aligning %s", name_.c_str());
-    // create publishers
-    pc_pub_ = nh_.advertise<sensor_msgs::PointCloud2>(name_ + "/pointcloud", 1, true);
-    pc_pcd_pub_ = nh_.advertise<sensor_msgs::PointCloud2>(name_ + "/transformed_pcd_cloud", 1, true);
-    path_pub_ = nh_.advertise<nav_msgs::Path>(name_ + "/path", 1, true);
+  ROS_INFO("Aligning %s", name_.c_str());
+  // create publishers
+  pc_pub_ = nh_.advertise<sensor_msgs::PointCloud2>(name_ + "/pointcloud", 1, true);
+  pc_pcd_pub_ = nh_.advertise<sensor_msgs::PointCloud2>(name_ + "/transformed_pcd_cloud", 1, true);
+  path_pub_ = nh_.advertise<nav_msgs::Path>(name_ + "/path", 1, true);
 
-    nh.param<std::string>(name_ + "/experiment_bag", bag_name_, "");
-    nh.param<bool>(name_ + "/transform_pcd", transform_pcd, false);
-    nh.param<std::string>(name_ + "/pcd_in_path", pcd_in_dir_, "");
-    nh.param<std::string>(name_ + "/pcd_out_path", pcd_out_dir_, "");
-    nh.param<int>(name_ + "/icp_iterations", iterations_, 100);
-    nh.param<int>(name_ + "/correspondence", correspondence_, 5);
-    nh.param<int>(name_ + "/ransac", ransac_, 0);
-    nh.param<double>(name_ + "/initial_rot", initial_rot_, 90.0);
-    nh.param<double>(name_ + "/initial_x", initial_x_, 0.0);
-    nh.param<double>(name_ + "/initial_y", initial_y_, 0.0);
+  nh.param<std::string>(name_ + "/experiment_bag", bag_name_, "");
+  nh.param<bool>(name_ + "/transform_pcd", transform_pcd, false);
+  nh.param<std::string>(name_ + "/pcd_in_path", pcd_in_dir_, "");
+  nh.param<std::string>(name_ + "/pcd_out_path", pcd_out_dir_, "");
+  nh.param<int>(name_ + "/icp_iterations", iterations_, 100);
+  nh.param<int>(name_ + "/correspondence", correspondence_, 5);
+  nh.param<int>(name_ + "/ransac", ransac_, 0);
+  nh.param<double>(name_ + "/initial_rot", initial_rot_, 90.0);
+  nh.param<double>(name_ + "/initial_x", initial_x_, 0.0);
+  nh.param<double>(name_ + "/initial_y", initial_y_, 0.0);
 
-    // Topics to read
-    std::vector<std::string> topics;
-    topics.push_back(std::string("/lio_sam/mapping/map_global"));
-    topics.push_back(std::string("/lio_sam/mapping/path"));
+  // Topics to read
+  std::vector<std::string> topics;
+  topics.push_back(std::string("/lio_sam/mapping/map_global"));
+  topics.push_back(std::string("/lio_sam/mapping/path"));
 
-    rosbag::Bag bag;
-    ROS_INFO("Opening %s", bag_name_.c_str());
-    bag.open(bag_name_, rosbag::bagmode::Read);
-    rosbag::View bag_view(bag, rosbag::TopicQuery(topics));
+  rosbag::Bag bag;
+  ROS_INFO("Opening %s", bag_name_.c_str());
+  bag.open(bag_name_, rosbag::bagmode::Read);
+  rosbag::View bag_view(bag, rosbag::TopicQuery(topics));
 
-    // Read experiment pointcloud and path
-    for (const auto& messageInstance : bag_view) {
-        sensor_msgs::PointCloud2ConstPtr pc_msg = messageInstance.instantiate<sensor_msgs::PointCloud2>();
-        if (pc_msg != NULL) {
-            pcl::fromROSMsg(*pc_msg, *experiment_cloud_);
-            ROS_INFO("Found %s experiment pointcloud.",  name_.c_str());
-        }
-
-        nav_msgs::Path::ConstPtr path_msg = messageInstance.instantiate<nav_msgs::Path>();
-        if (path_msg != NULL) {
-            path_ = *path_msg;
-            ROS_INFO("Found %s experiment path.",  name_.c_str());
-        } 
+  // Read experiment pointcloud and path
+  for (const auto& messageInstance : bag_view)
+  {
+    sensor_msgs::PointCloud2ConstPtr pc_msg = messageInstance.instantiate<sensor_msgs::PointCloud2>();
+    if (pc_msg != NULL)
+    {
+      pcl::fromROSMsg(*pc_msg, *experiment_cloud_);
+      ROS_INFO("Found %s experiment pointcloud.", name_.c_str());
     }
+
+    nav_msgs::Path::ConstPtr path_msg = messageInstance.instantiate<nav_msgs::Path>();
+    if (path_msg != NULL)
+    {
+      path_ = *path_msg;
+      ROS_INFO("Found %s experiment path.", name_.c_str());
+    }
+  }
 }
 
 bool misaligned::icp()
 {
-   PointCloudT::Ptr cloud_tr (new PointCloudT);
+  PointCloudT::Ptr cloud_tr(new PointCloudT);
   *cloud_tr = *experiment_cloud_;
 
-  Eigen::Affine3f initial_transformation_affine;
-  Eigen::Matrix4d initial_transformation_matrix;
+  Eigen::Affine3f initial_tf_affine;
+  Eigen::Matrix4d initial_tf_matrix;
 
-  initial_transformation_affine = pcl::getTransformation(initial_x_, initial_y_, 0, 0, 0, initial_rot_*3.14/180);
-  initial_transformation_matrix = initial_transformation_affine.cast<double>().matrix();
-  pcl::transformPointCloud (*cloud_tr, *experiment_cloud_, initial_transformation_matrix);
+  initial_tf_affine = pcl::getTransformation(initial_x_, initial_y_, 0, 0, 0, initial_rot_ * 3.14 / 180);
+  initial_tf_matrix = initial_tf_affine.cast<double>().matrix();
+  pcl::transformPointCloud(*cloud_tr, *experiment_cloud_, initial_tf_matrix);
 
   pcl::VoxelGrid<PointT> downSizeFilter;
   downSizeFilter.setLeafSize(10.0, 10.0, 10.0);
@@ -93,16 +92,13 @@ bool misaligned::icp()
 
   ROS_INFO("%s ICP finished", name_.c_str());
 
-  if (icp.hasConverged ())
+  if (icp.hasConverged())
   {
-    // std::cout << "\nICP has converged, score is " << icp.getFitnessScore () << std::endl;
-    // std::cout << " : experiment_cloud_ -> baseline_cloud_" << std::endl;
-    Eigen::Matrix4d icp_tf_matrix = icp.getFinalTransformation ().cast<double>();
-    // print4x4Matrix (icp_tf_matrix);
+    Eigen::Matrix4d icp_tf_matrix = icp.getFinalTransformation().cast<double>();
 
     // transform the experiment cloud to the correct pose
-    Eigen::Matrix4d resultant_tf = icp_tf_matrix * initial_transformation_matrix;
-    pcl::transformPointCloud (*cloud_tr, *experiment_cloud_, resultant_tf);
+    Eigen::Matrix4d resultant_tf = icp_tf_matrix * initial_tf_matrix;
+    pcl::transformPointCloud(*cloud_tr, *experiment_cloud_, resultant_tf);
 
     Eigen::Vector4d initial_position;
     Eigen::Vector4d transformed_position;
@@ -111,7 +107,7 @@ bool misaligned::icp()
 
     ROS_INFO("Transforming poses...");
     // transform pose from the local frame to the global frame
-    for (auto & pose : path_.poses)
+    for (auto& pose : path_.poses)
     {
       // Transforming position
       initial_position[0] = pose.pose.position.x;
@@ -125,22 +121,21 @@ bool misaligned::icp()
       pose.pose.position.z = transformed_position[2];
 
       // Transforming orientation
-      tf2::Matrix3x3 tf_rot_mat(
-          resultant_tf(0, 0), resultant_tf(0, 1), resultant_tf(0, 2),
-          resultant_tf(1, 0), resultant_tf(1, 1), resultant_tf(1, 2),
-          resultant_tf(2, 0), resultant_tf(2, 1), resultant_tf(1, 2));
-        
+      tf2::Matrix3x3 tf_rot_mat(resultant_tf(0, 0), resultant_tf(0, 1), resultant_tf(0, 2), resultant_tf(1, 0),
+                                resultant_tf(1, 1), resultant_tf(1, 2), resultant_tf(2, 0), resultant_tf(2, 1),
+                                resultant_tf(1, 2));
+
       tf2::fromMsg(pose.pose.orientation, quat);
       tf2::Matrix3x3 initial_rotation(quat);
 
       transformed_orientation = initial_rotation * tf_rot_mat;
-      
+
       transformed_orientation.getRotation(quat);
       pose.pose.orientation = tf2::toMsg(quat);
     }
 
     ROS_INFO("%s publishing messages", name_.c_str());
-    
+
     path_pub_.publish(path_);
 
     sensor_msgs::PointCloud2 pc_msg;
@@ -151,85 +146,75 @@ bool misaligned::icp()
   }
   else
   {
-    PCL_ERROR ("\nICP has not converged.\n");
+    PCL_ERROR("\nICP has not converged.\n");
     return false;
   }
 }
 
 void misaligned::write_to_csv(std::string dir)
 {
-    std::ofstream csv_file;
-    csv_file.open (dir + name_ + ".csv");
-    csv_file << "#timestamp, tx, ty, tz, qx, qy, qz, qw\n";
+  std::ofstream csv_file;
+  csv_file.open(dir + name_ + ".csv");
+  csv_file << "#timestamp, tx, ty, tz, qx, qy, qz, qw\n";
 
-    for (auto & pose : path_.poses) {
-        csv_file <<
-            pose.header.stamp << ", " <<
-            pose.pose.position.x << ", " <<
-            pose.pose.position.y << ", " <<
-            pose.pose.position.z << ", " <<
-            pose.pose.orientation.x << ", " <<
-            pose.pose.orientation.y << ", " <<
-            pose.pose.orientation.z << ", " <<
-            pose.pose.orientation.w << std::endl;
-    }
-    csv_file.close();
+  for (auto& pose : path_.poses)
+  {
+    csv_file << pose.header.stamp << ", " << pose.pose.position.x << ", " << pose.pose.position.y << ", "
+             << pose.pose.position.z << ", " << pose.pose.orientation.x << ", " << pose.pose.orientation.y << ", "
+             << pose.pose.orientation.z << ", " << pose.pose.orientation.w << std::endl;
+  }
+  csv_file.close();
 }
 
 void misaligned::transform_pcd_files()
 {
-    std::cout << "Transforming PCD files...\n";
+  std::cout << "Transforming PCD files...\n";
 
-    // create directory and remove old files;
-    int unused = system((std::string("exec rm -r ") + pcd_out_dir_).c_str());
-    unused = system((std::string("mkdir -p ") + pcd_out_dir_).c_str());
+  // create directory and remove old files;
+  int unused = system((std::string("exec rm -r ") + pcd_out_dir_).c_str());
+  unused = system((std::string("mkdir -p ") + pcd_out_dir_).c_str());
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_source (new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_transformed (new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr full_transformed_cloud (new pcl::PointCloud<pcl::PointXYZ>);
-    Eigen::Affine3f transformation_affine;
-    Eigen::Matrix4d transformation_matrix;
-    geometry_msgs::PoseStamped pose;
-    double roll, pitch, yaw;
-    std::string pcd_path;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_source(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_transformed(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr full_transformed_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  Eigen::Affine3f transformation_affine;
+  Eigen::Matrix4d transformation_matrix;
+  geometry_msgs::PoseStamped pose;
+  double roll, pitch, yaw;
+  std::string pcd_path;
 
-    int len = path_.poses.size();
-    for(int i=0; i<len && ros::ok(); i++) { 
-      char file_name_buffer[11]; // xxxxxx.pcd
-      sprintf(file_name_buffer, "%06d.pcd", i);
-      pcd_path = pcd_in_dir_ + std::string(file_name_buffer);
-      
-      if (pcl::io::loadPCDFile<pcl::PointXYZ> (pcd_path, *cloud_source) == -1) //* load the file
-      {
-        ROS_ERROR("Couldn't read file %s \n", pcd_path.c_str());
-        return;
-      }
+  int len = path_.poses.size();
+  for (int i = 0; i < len && ros::ok(); i++)
+  {
+    char file_name_buffer[11];  // xxxxxx.pcd
+    sprintf(file_name_buffer, "%06d.pcd", i);
+    pcd_path = pcd_in_dir_ + std::string(file_name_buffer);
 
-      pose = path_.poses[i];
-      tf::Quaternion q(
-        pose.pose.orientation.x,
-        pose.pose.orientation.y,
-        pose.pose.orientation.z,
-        pose.pose.orientation.w);
-      tf::Matrix3x3 m(q);
-      m.getRPY(roll, pitch, yaw);
-
-      transformation_affine = pcl::getTransformation(
-        pose.pose.position.x,
-        pose.pose.position.y, 
-        pose.pose.position.z,
-        roll, pitch, yaw);
-
-      transformation_matrix = transformation_affine.cast<double>().matrix();
-
-      pcl::transformPointCloud(*cloud_source, *cloud_transformed, transformation_matrix);
-      *full_transformed_cloud += *cloud_transformed;
-
-      sensor_msgs::PointCloud2 pc_msg;
-      pcl::toROSMsg(*full_transformed_cloud, pc_msg);
-      std::cout << pc_msg.header.frame_id;
-
-      pcl::io::savePCDFile(pcd_path, *cloud_transformed);
-      std::cout << pcd_path << " saved.\n";
+    if (pcl::io::loadPCDFile<pcl::PointXYZ>(pcd_path, *cloud_source) == -1)  //* load the file
+    {
+      ROS_ERROR("Couldn't read file %s \n", pcd_path.c_str());
+      return;
     }
+
+    pose = path_.poses[i];
+    tf::Quaternion q(pose.pose.orientation.x, pose.pose.orientation.y, pose.pose.orientation.z,
+                     pose.pose.orientation.w);
+    tf::Matrix3x3 m(q);
+    m.getRPY(roll, pitch, yaw);
+
+    transformation_affine =
+        pcl::getTransformation(pose.pose.position.x, pose.pose.position.y, pose.pose.position.z, roll, pitch, yaw);
+
+    transformation_matrix = transformation_affine.cast<double>().matrix();
+
+    pcl::transformPointCloud(*cloud_source, *cloud_transformed, transformation_matrix);
+    *full_transformed_cloud += *cloud_transformed;
+
+    sensor_msgs::PointCloud2 pc_msg;
+    pcl::toROSMsg(*full_transformed_cloud, pc_msg);
+    std::cout << pc_msg.header.frame_id;
+
+    pcl::io::savePCDFile(pcd_path, *cloud_transformed);
+    std::cout << pcd_path << " saved.\n";
+  }
 }
